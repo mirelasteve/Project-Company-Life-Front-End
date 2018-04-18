6371bbe169af09ad8143d661ae3ec952be8b275d
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2018-04-18 20:51:50 +0300

    changed imports in the components as some of them were dublicated

--- lifeProject/src/app/app.module.ts
+++ lifeProject/src/app/app.module.ts
@@ -2,25 +2,20 @@ import { Inject, Input, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ViewChild } from '@angular/core/src/metadata/di';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS, MatDialog, MatDialogRef } from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
-import { DialogComponent, LoginModalComponent } from './login-modal/login-modal.component';
 import { NavComponent } from './nav/nav.component';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS, MatNativeDateModule, MatInputModule, MatTableModule } from '@angular/material';
 import { MatDialogModule } from '@angular/material/dialog';
 import { LoginModalComponent, DialogComponent } from './login-modal/login-modal.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ViewChild } from '@angular/core/src/metadata/di';
 import { RegisterComponent, RegistrationComponent } from './register/register.component';
 import { CareersComponent } from './careers/careers.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -31,10 +26,22 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import { JobDetailsComponent } from './job-details/job-details.component';
+import { AdminComponent } from './admin/admin.component';
+import { UsersComponent } from './admin/users/users.component';
+import { AdsComponent } from './admin/ads/ads.component';
+import { ApplicationsComponent } from './admin/applications/applications.component';
+import { ContactsComponent } from './admin/contacts/contacts.component';
+import { LinksComponent } from './admin/links/links.component';
+import { HomeComponent } from './home/home.component';
 
- ];
 @NgModule({
   declarations: [
+    AdminComponent,
+    UsersComponent,
+    AdsComponent,
+    ApplicationsComponent,
+    LinksComponent,
+    ContactsComponent,
     AppComponent,
     DialogComponent,
     NavComponent,
@@ -44,6 +51,7 @@ import { JobDetailsComponent } from './job-details/job-details.component';
     CareersComponent,
     ContactComponent,
     JobDetailsComponent,
+    HomeComponent,
   ],
   imports: [
     MatTableModule,
