01b4ea4392e8b02b9dc6f4db375e65922725a5be
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2018-04-22 15:13:39 +0300

    created interfaces for contact details, job ads, job applications, job types and links

---
+++ lifeProject/src/app/models/links.ts
@@ -0,0 +1,9 @@
+export interface ILinks {
+    id: number;
+    name: string;
+    linkTarget: string;
+    iconLink: string;
+    hidden: null | string;
+    createdAt: Date;
+    updatedAt: Date;
+}
