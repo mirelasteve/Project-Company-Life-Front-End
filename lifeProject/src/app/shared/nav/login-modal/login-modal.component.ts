3073598160378b26065fec160698dcc4e9ec2b88
Mirela <mirelasteve@abv.bg>
2018-04-30 21:57:06 +0300

    filter table change

--- lifeProject/src/app/shared/nav/login-modal/login-modal.component.ts
+++ lifeProject/src/app/shared/nav/login-modal/login-modal.component.ts
@@ -1,3 +1,4 @@
+
 import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material';
@@ -18,7 +19,7 @@ import { NavComponent } from '../nav.component';
 export class LoginModalComponent {
 
   constructor(public dialog: MatDialog) {}
-  public openDialog(): void {
+  private openDialog(): void {
     console.log('clicked');
     const dialogRef = this.dialog.open(DialogComponent, {
       height: '150px',
@@ -35,16 +36,18 @@ export class LoginModalComponent {
   selector: 'app-dialog',
   templateUrl: './dialog.html',
 })
-export class DialogComponent {
+export class DialogComponent   {
   public username;
   public password;
-  constructor(public dialogRef: MatDialogRef<DialogComponent>,
+  constructor( public dialogRef: MatDialogRef<DialogComponent>,
               // public navComponent: NavComponent,
-              private http: HttpClient,
-              private activatedRoute: ActivatedRoute,
-              private loginService: LoginService,
-              private toastr: ToastrService,
-              private router: Router) { }
+               private http: HttpClient,
+               private activatedRoute: ActivatedRoute,
+               private loginService: LoginService,
+               private toastr: ToastrService,
+               private router: Router) {
+
+ }
   private login(): void {
     this.dialogRef.close();
     this.dialogRef.afterClosed().subscribe((result) => {
@@ -55,7 +58,7 @@ export class DialogComponent {
                                { observe: 'response', responseType: 'json' })
                                .subscribe((x: HttpResponse<{token: string}>) => {
 
-        localStorage.setItem('access_token', x.token);
+        localStorage.setItem('access_token', x.body.token);
         localStorage.setItem('user_name', this.username);
         this.toastr.success(` registered!`);
         // this.navComponent.ngOnInit();
@@ -70,9 +73,8 @@ export class DialogComponent {
 
     });
 
-
   }
-  private reload(){
+  private reload() {
     // location.replace('/');
   }
   private onNoClick(): void {
