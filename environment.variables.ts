f22edf7585379c060f97e58c7e8ec404cbca9cf3
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2018-04-30 21:54:35 +0300

    added environment variables file to root folder

---
+++ environment.variables.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    homePageImage: './lifeProject/src/assets/images/background-home-page.jpg',
+    homePageTextArea: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
